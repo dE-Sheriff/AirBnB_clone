63a911a07b04209e9f50d8f109e87dd264e1a8b9
Sheriff Sarafa <sheriffsarafa@gmail.com>
2024-03-25 06:05:07 +0100

    edit on AirBNB

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
  #!/usr/bin/python3
-from base_model import BaseModel
+""" A state class """
 
-class Sate(BaseModel):
-    """ """
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """ Managing state class attributes """
     name = ""
--- models/user.py
+++ models/user.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+
 from models.base_model import BaseModel
 
 class User(BaseModel):
-    """ """
+    """Class instance for User"""
     firs_name = ""
     last_name = ""
     email = ""
     password = ""
+    
+    def __init__(self, *args, **kwargs):
+        """Initializes User"""
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+        super(User, self).__init__(*args, **kwargs)
